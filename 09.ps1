9dced37a190662d12a860db222e8b580cc08bafe
GerPujRua <95307495+GerPujRua@users.noreply.github.com>
2022-02-11 12:17:36 +0100

    9

--- 09.ps1
+++ 09.ps1
@@ -1,5 +1,3 @@
 #si dos numeros passats per parametre a l'script son iguals mostra OK. Si son diferents mostra KO
-$num1=$args[0]
-$num2=$args[1]
-write-host $num1 $num2
+param ($num1, $num2)
 if ($num1 -eq $num2){ Write-Output "OK"} else { Write-Output "KO"}
