1d2185587fd3c533f1c6e18b95adc9dbe1178a53
GerPujRua <95307495+GerPujRua@users.noreply.github.com>
2022-02-11 12:08:08 +0100

    9

--- 09.ps1
+++ 09.ps1
@@ -1,2 +1,2 @@
 #si dos numeros passats per parametre a l'script son iguals mostra OK. Si son diferents mostra KO
-if (1 -eq 1){ Write-Output "OK"}
+if (1 -eq 1){ Write-Output "OK"} else { Write-Output "KO"}
